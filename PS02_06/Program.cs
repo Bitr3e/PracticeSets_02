d22c1948130bf7db0d6c2ad7e90b97b26831a81b
John Brence Condesa <condesajohnbrence@gmail.com>
2024-08-28 00:57:40 +0800

    done

---
+++ PS02_06/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PracticeSets_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        bool isPalindrome = IsPalindrome(input);
+
+        if (isPalindrome)
+        {
+            Console.WriteLine("The string is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine("The string is not a palindrome.");
+        }
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        string cleanedStr = new string(str
+            .Select(c => char.ToLower(c))
+            .ToArray());
+
+        string reversedStr = new string(cleanedStr.Reverse().ToArray());
+
+        return cleanedStr == reversedStr;
+        }
+    }
+}
