f1a474ab4931acdb1025b287708c40c5adcc502e
John Brence Condesa <condesajohnbrence@gmail.com>
2024-08-28 00:02:27 +0800

    done

---
+++ PS02_05/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PracticeSets_02
+{
+    class Program //RiTVent Test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any character: ");
+            char letter = char.Parse(Console.ReadLine());
+
+            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+            {
+                Console.WriteLine($"The letter {letter} is a vowel.");
+            }
+
+            else if (Char.IsLetter(letter))
+            {
+                Console.WriteLine($"The letter {letter} is a consonant.");
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+    }
+}
