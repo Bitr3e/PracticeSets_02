dd032b45524418d738a4cd51862a9b0065afae96
Calaisha Mae S. Ducao <109143325+PsychoQuake7@users.noreply.github.com>
2024-08-28 11:14:42 +0800

    Add files via upload

---
+++ PS02_08/PS02_08/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS02_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            int sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                int digit = input[i] - '0';
+
+                sum += digit;
+            }
+
+            Console.WriteLine($"The sum of the digits is: {sum}");
+
+        }
+    }
+}
---
+++ PS02_09/PS02_09/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS02_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter numbers separated by spaces: ");
+            string input = Console.ReadLine();
+
+            string[] stringNumbers = input.Split(' ');
+
+            int[] numbers = Array.ConvertAll(stringNumbers, int.Parse);
+
+            int sum = 0;
+
+            int i = 0;
+
+            while (i < numbers.Length)
+            {
+                sum += numbers[i];
+                i++;
+            }
+
+            Console.WriteLine($"The sum of the numbers is: {sum}");
+        }
+    }
+}
